6a0d48cd2fa359a7aa45388bd7085e2de034a1a1
JonnyRoll <Jjrollansky@hotmail.com>
2026-05-20 21:46:04 -0400

    This improves the documentation on the piece function file. using proper documentation still and informative commenting.

--- piece.py
+++ piece.py
@@ -12,6 +12,7 @@ class Piece(ABC):
     black_player_pieces = set()
     white_player_pieces = set()
     def __init__(self, name:str ,vertical_axis:int, horizontal_axis:int, value:int):
+        """ This is the constructor for the piece object, it assigns the values to all the variables"""
         self.name = name
         self.vertical_axis = vertical_axis
         self.horizontal_axis = horizontal_axis
@@ -19,9 +20,11 @@ class Piece(ABC):
         self.color = True if name[-1] == 'W' else False
 
     def __str__(self):
+        """This function returns the string representation of the piece object"""
         return self.name
 
     def __repr__(self):
+        """This function returns the string representation of the piece object while printing in a loop"""
         return self.name
 
     # this is the abstract method!
@@ -37,6 +40,12 @@ class Piece(ABC):
 
 
     def valid_move_diagonal(self, vertical_destination: int, horizontal_destination: int) -> bool:
+        """
+        This function validates a diagonal movement, it does so by checking if the amount you want to move vertical = the amount moved horizontally
+        afterward it checks if all the blocks the piece must move passed in empty.
+        If the move is valid then the definition will return True
+        If the move is not valid then it will return False
+        """
         amount_moved_vertical = vertical_destination - self.vertical_axis
         amount_moved_horizontal = horizontal_destination - self.horizontal_axis
         # if we do not move from at least 1 axis
@@ -46,22 +55,31 @@ class Piece(ABC):
         elif abs(amount_moved_vertical) != abs(amount_moved_horizontal):
             return False
 
+        # will determine if we are moving up higher on the board (negative) for vertical and move left on the board (negative)
         vertical_direction = amount_moved_vertical // abs(amount_moved_vertical)
         horizontal_direction = amount_moved_horizontal // abs(amount_moved_horizontal)
+        #checks all the pieces the piece has to pass over
         for i in range(1, abs(amount_moved_vertical)):
             if Piece.chess_board[self.vertical_axis + (i * vertical_direction)][self.horizontal_axis + (i * horizontal_direction)] == 0:
                 continue
-            else: return False
+            else: return False # there is a piece in the way
+
         if Piece.chess_board[vertical_destination][horizontal_destination] == 0:
             return True
-        elif Piece.chess_board[vertical_destination][horizontal_destination].color != self.color:
+        elif Piece.chess_board[vertical_destination][horizontal_destination].color != self.color: # if we have to capture an opponent's piece
             return True
-        else:
+        else: # There is one of their pieces on the board
             return False
 
 
-    # improve this to one path! # dont need this method!
+
     def valid_move_vert_and_horz(self, vertical_destination: int, horizontal_destination: int) -> bool:
+        """
+        This method checks if the 'straight' lined movement is valid,
+        first we decide if we are going to check if the piece is moving vertically or horizontally.
+        if the piece is moving vertically we will move along the vertical axis to check if all squares inbetween the original and desired square is empty (allowing for a valid move)
+        if the piece is found to move horizontally the same action will be done as the vertical check (but just incrementing the horizontal direction instead)
+        """
         amount_moved_vertically = vertical_destination - self.vertical_axis
         amount_moved_horizontally = horizontal_destination - self.horizontal_axis
         # if the piece has not moved
@@ -74,38 +92,50 @@ class Piece(ABC):
         if amount_moved_vertically == 0:
             # finds the direction of movement for the piece
             horizontal_direction = amount_moved_horizontally // abs(amount_moved_horizontally)
-            # checks iof the coast is clear
+            # checks every intermediate square and varifies it is empty
             for i in range(1, abs(amount_moved_horizontally)):
                 if Piece.chess_board[self.vertical_axis][self.horizontal_axis + (i * horizontal_direction)] == 0:
                     continue
-                else:
+                else: # if the square has a piece on it
                     return False
+            # makes sure the desired square deos not have a piece of the same team on it.
             if Piece.chess_board[self.vertical_axis][horizontal_destination] == 0:
                 return True
             elif Piece.chess_board[self.vertical_axis][horizontal_destination].color != self.color:
                 return True
             else:
                 return False
+
         # then the piece moves in the vertical direction (horizontal destination = current horizontal)
         elif amount_moved_horizontally == 0:
             vertical_direction = amount_moved_vertically // abs(amount_moved_vertically)
-            # checks iof the coast is clear
+            # checks every intermediate square and varifies it is empty
             for i in range(1, abs(amount_moved_vertically)):
                 if Piece.chess_board[self.vertical_axis + (i * vertical_direction)][self.horizontal_axis] == 0:
                     continue
-                else:
+                else: # if the square (we are currently checking) has a piece on it
                     return False
+            # makes sure the desired square deos not have a piece of the same team on it.
             if Piece.chess_board[vertical_destination][self.horizontal_axis] == 0:
                 return True
             elif Piece.chess_board[vertical_destination][horizontal_destination].color != self.color:
                 return True
             else:
                 return False
-        # if we get here we had a problem!
-        else: return False
+
+        # if we get here we had a problem! basically not possible
+        else:
+            print('you should not have reached here')
+            return False
 
 
     def place_piece(self, vertical_desired: int, horizontal_desired: int) -> bool:
+        """
+        This function defines how a piece will be moved from its current position on the board to ints new position on the board
+        additionally the function stores the old position of the piece, in the case the next move is not valid (still is in check), this allows us to revert back to the old game state if needed.
+        Additionally, in the case that we must capture a piece on this move. A flag will go up indicating we have killed a piece in this action, then the newly killed piece will be placed in a temporary holder.
+        if the move is valid the captured piece will be discarded, if the move is invalid however we will place the piece back in the correct sett before going back to the old game state.
+        """
         # removing the piece from old index
         Piece.chess_board[self.vertical_axis][self.horizontal_axis] = 0
         # storing old indexes
@@ -150,6 +180,9 @@ class Piece(ABC):
 
 
     def current_king(self) -> Piece:
+        """
+        This function will check the correct set and find the king piece of said set
+        """
         # finding the position of the current king
         current_set = Piece.white_player_pieces if self.color else Piece.black_player_pieces
         current_king = 'KING_W' if self.color else 'KING_B'
@@ -158,12 +191,17 @@ class Piece(ABC):
             if piece.name == current_king:
                 return piece
 
-            # if we get here there is a major problem!
+        # if we get here there is a major problem! (means the king does not exist)
         print(f'{current_king} could not be found')
         return None
 
     def king_in_check(self) -> bool:
-        # deciding which sett is the enemy set!
+        """
+        This function will check the correct set and determine if any piece from the enemy set can reach the kings position
+        if so the king is in check.
+
+        :return: if the king of the set is in check (if returned true then the king is in check)
+        """
         opponents_set = Piece.black_player_pieces if self.color else Piece.white_player_pieces
         king = self.current_king()
         for piece in opponents_set:
@@ -173,83 +211,3 @@ class Piece(ABC):
 
         #this indicates the king is not in check!
         return False
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # this might not be needed at all lol
-#     def valid_knight_move(self, vertical_destination: int, horizontal_destination: int) -> bool:
-#         # how much movement the piece has in each direction
-#         vertical_movement = vertical_destination - self.vertical_axis
-#         horizontal_movement = horizontal_destination - self.horizontal_axis
-#
-#         # if we are trying to put the piece outside the board
-#         if not(0 <= vertical_destination < 8) and not(0 <= horizontal_destination < 8):
-#             return False
-#
-#         # if either direction is zero the L shape movement is not satisfied
-#         elif vertical_movement == 0 or horizontal_movement == 0:
-#             return False
-#
-#         # the L shape is satisfied
-#         elif (abs(vertical_movement) == 2 and abs(horizontal_movement) == 1) or (abs(vertical_movement) == 1 and abs(horizontal_movement) == 2):
-#             # the position is empty, and we can place the piece there with no extra action!
-#             if Piece.chess_board[vertical_destination][horizontal_destination] == 0:
-#                 return True
-#             # we must capture the piece from the other team before being able to place
-#             elif Piece.chess_board[vertical_destination][horizontal_destination].color != self.color:
-#                 return True
-#             # the piece in the position is the same color as the piece we are currently trying to move
-#             else: return False # Piece.chess_board[vertical_destination][horizontal_destination].color == self.color
-#
-#         # if we get here then there was an input error
-#         return False
-# # this might not be needed
-#     def valid_move_pawn(self, vertical_destination: int, horizontal_destination: int) -> bool:
-#         #finding how much the total movement is
-#         vertical_movement = vertical_destination - self.vertical_axis
-#         horizontal_movement = horizontal_destination - self.horizontal_axis
-#
-#         #deciding which direction the piece can move based on color
-#         # NOTE if self.color is true we are on white team
-#         valid_direction = -1 if self.color else 1
-#
-#         # if the pawn moves in its traditional manner (moves one square vertically)
-#         if vertical_movement == valid_direction and horizontal_movement == 0:
-#             return Piece.chess_board[vertical_destination][horizontal_destination] == 0
-#
-#         # in the case it's the pawn first move it can jump two pieces forward NOTE the two square need to be empty!
-#         elif vertical_movement == (2*valid_direction) and horizontal_movement == 0 and self.first_move:
-#             #checking if both square are free!
-#             return (Piece.chess_board[vertical_destination][horizontal_destination] == 0
-#                     and Piece.chess_board[self.vertical_axis + valid_direction][horizontal_destination] == 0)
-#
-#         #this is when a pawn wants to capture its diagonal counterpart
-#         elif vertical_movement == valid_direction and abs(horizontal_movement) == 1:
-#             # if the diagonal square is not occupied by an enemy piece
-#             if (Piece.chess_board[vertical_destination][horizontal_destination] == 0
-#                 or Piece.chess_board[vertical_destination][horizontal_destination].color == self.color):
-#
-#                 return False
-#             # the diagonal piece is occupied by an enemy piece
-#             else:
-#                 return True
-#         # if we get here the move is not valid!
-#         return False
-#
-#
-
-
-
-
-
